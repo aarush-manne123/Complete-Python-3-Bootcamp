8ed44676eef8cc143e313ddcdd2c4d00feb0993e
aarush-manne123 <rushi.manne@gmail.com>
2026-03-07 20:35:53 -0800

    Fix color style in HTML header for welcome message

--- app.py
+++ app.py
@@ -80,8 +80,8 @@ def games():
             html = f"""
             <html>
 			<head>
-				<h1 style="color:light-blue">Welcome to</h1>
-				<t style="color:light-blue">LUNAR</t>
+				<h1 style="color:lightblue">Welcome to</h1>
+				<t style="color:lightblue">LUNAR</t>
 			</head>
             <body style="background-image: url('https://img.freepik.com/free-photo/illustration-cosmic-background-with-orange-neon-laser-lights_181624-19567.jpg?semt=ais_rp_50_assets&w=740&q=80'); background-size: cover;">
                 <p style="color: teal;">HOW TO USE:</p>
